3ef041f50346ed2c2fd8214877a716820e616c2f
John Hitz <johnh@whodapet.com>
2016-09-12 19:09:19 -0500

    First commit

---
+++ hello/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const (
+	x = "WTF? Over!"
+	y = "What a world!"
+)
+
+func main() {
+	fmt.Println(x)
+	x := "Hello World!"
+	fmt.Println(x)
+	fmt.Println(y)
+}
---
+++ salutation/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "John"
+	fmt.Println("Hello", name)
+}
