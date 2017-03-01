e1848680e6142ecced820dae274a8dd916bc02c6
John Hitz <johnh@whodapet.com>
2017-03-01 15:33:44 -0600

    Add 02_numeral-system 01_decimal and 02_binary

---
+++ getting-started/03_hexadecimal/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d \t %b \t %x \n", 42, 42, 42)
+	fmt.Printf("%d \t %b \t %x \n", 42, 42, 42)
+	fmt.Printf("%d \t %b \t %#x \n", 42, 42, 42)
+	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
+	fmt.Printf("%d \t %b \t %X \n", 42, 42, 42)
+}
